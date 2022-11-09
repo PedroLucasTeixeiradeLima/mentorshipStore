e4e6f9144ef98afd1b6e7951316614819197ad1e
Pedro Lucas Teixeira de Lima <Pedro.Lucas.Teixeira.de.Lima@ibm.com>
2022-11-09 14:43:59 -0300

    construindo routes

--- index.js
+++ index.js
@@ -2,6 +2,40 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const router = express.Router();
+
+router.get('/', (request, response) => {
+  response.send('<h2> Hello world! </h2>');
+});
+
+const products = [];
+
+// POST always have = BODY -> all the data comes in body
+router.post('/product', (request, response) => {
+  const body = request.body;
+  
+  products.push(body);
+
+  response.status(201).send();
+});
+
+router.get('/product', (request, response) =>{
+  response.send(products);
+})
+
+app.use(router);
+
 app.listen(8000, () => {
   console.log('I\'m running at port 8000');
 });
+
+// PRODUCT
+// {
+//   id: number,
+//   name: string,
+//   description: string,
+//   price: number,
+//   quantity: number,
+// }
