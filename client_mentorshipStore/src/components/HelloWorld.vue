487a8583ead02a30acd2abdadfe6fb4a2a37d26c
Pedro Lucas Teixeira de Lima <Pedro.Lucas.Teixeira.de.Lima@ibm.com>
2023-03-21 13:29:26 -0300

    Practicing Vue before inserting true elements of the project

--- client_mentorshipStore/src/components/HelloWorld.vue
+++ client_mentorshipStore/src/components/HelloWorld.vue
@@ -1,20 +1,31 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+<script>
+
+export default{
+  data(){
+    return {
+      count: 0
+    }
+  },
+
+  methods:{
+    increment(){
+      this.count++
+    },
+    decrement(){
+      this.count--
+    }
+  },
+
+  
+}
+
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h1>{{ count }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
   </div>
 </template>
 
