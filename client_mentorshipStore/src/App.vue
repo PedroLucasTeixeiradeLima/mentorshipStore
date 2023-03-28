9669bd8dcda6b15daada912bd7b05ee5f1480d4c
Pedro Lucas Teixeira de Lima <Pedro.Lucas.Teixeira.de.Lima@ibm.com>
2023-03-28 14:03:02 -0300

    Modifying vue components

--- client_mentorshipStore/src/App.vue
+++ client_mentorshipStore/src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1> Hello World! </h1>
+  
 
-    <div class="wrapper">
-      <HelloWorld msg="You really did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
