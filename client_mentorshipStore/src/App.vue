301f7dda620077cd088f1768363237479e0b91e8
Pedro Lucas Teixeira de Lima <Pedro.Lucas.Teixeira.de.Lima@ibm.com>
2023-03-28 15:07:35 -0300

    Functional Cards, but without integration

--- client_mentorshipStore/src/App.vue
+++ client_mentorshipStore/src/App.vue
@@ -1,14 +1,82 @@
-<script setup>
+<script>
+export default {
+  data(){
+    return{
+      // THIS WILL BE RETURNED FROM OUR API
+      products: [
+        {
+          id: 1,
+          title: 'Mocked product 1',
+          description: 'Mocked description 1',
+          price: 10,
+          quantity: 12
+        },
+        {
+          id: 2,
+          title: 'Mocked product 2',
+          description: 'Mocked description 2',
+          price: 10,
+          quantity: 12
+        },
+        {
+          id: 3,
+          title: 'Mocked product 3',
+          description: 'Mocked description 3',
+          price: 10,
+          quantity: 12
+        },
+        {
+          id: 4,
+          title: 'Mocked product 4',
+          description: 'Mocked description 4',
+          price: 10,
+          quantity: 12
+        },
+        {
+          id: 5,
+          title: 'Mocked product 5',
+          description: 'Mocked description 5',
+          price: 10,
+          quantity: 12
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-  <h1> Hello World! </h1>
-  
+  <div v-for="product in products" :key="product.id" class="card-container">
+    <div class="card-header">
+      <h2>{{ product.title }}</h2>
+      <button>X</button>
+    </div>
 
+    <p class="card-description">{{ product.description }}</p>
 
+    <div class="card-footer">
+      <span>R$ {{ product.price }}</span>
+      <span>Qty: {{ product.quantity }}</span>
+    </div>
+  </div>
 </template>
 
+
+
 <style scoped>
+.card-container {
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 5px;
+  min-width: 300px;
+  margin: 10px;
+}
+.card-header, .card-footer {
+  display: flex;
+  justify-content: space-between;
+}
+
+
 header {
   line-height: 1.5;
 }
