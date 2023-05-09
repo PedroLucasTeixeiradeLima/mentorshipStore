31d66e4b7f5b560c1db71b4366f3b7505adaf7ef
Pedro Lucas Teixeira de Lima <Pedro.Lucas.Teixeira.de.Lima@ibm.com>
2023-05-09 14:09:08 -0300

    feat: setting backend to serve frontend through a new route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const productsRouter = require(`./routes/products`);
@@ -7,7 +8,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client_mentorshipStore','dist')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.use(productsRouter);
+
 
 app.listen(8000, () => {
   console.log('I\'m running at port 8000');
